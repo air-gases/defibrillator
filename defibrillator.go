56798ea8b219420c9e00144a4ca0941d693fddcd
Aofei Sheng <aofei@aofeisheng.com>
2018-08-17 14:27:35 +0800

    refactor: make gases configurable

--- defibrillator.go
+++ defibrillator.go
@@ -6,15 +6,21 @@ import (
 	"github.com/sheng/air"
 )
 
-// Gas is used to recover from panics.
-func Gas(next air.Handler) air.Handler {
-	return func(req *air.Request, res *air.Response) (err error) {
-		defer func() {
-			if r := recover(); r != nil {
-				err = fmt.Errorf("%v", r)
-			}
-		}()
+// GasConfig is a set of configurations for the `Gas()`.
+type GasConfig struct {
+}
 
-		return next(req, res)
+// Gas returns an `air.Gas` that is used to recover from panics based on the gc.
+func Gas(gc GasConfig) air.Gas {
+	return func(next air.Handler) air.Handler {
+		return func(req *air.Request, res *air.Response) (err error) {
+			defer func() {
+				if r := recover(); r != nil {
+					err = fmt.Errorf("%v", r)
+				}
+			}()
+
+			return next(req, res)
+		}
 	}
 }
